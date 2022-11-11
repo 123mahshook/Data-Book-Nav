924d0756afcd97c5185cb6e0340b94babf010005
123mahshook <mahshookrk@gmail.com>
2022-11-11 13:39:00 +0530

    first commit

---
+++ app/src/main/java/com/filepicker/databook/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.filepicker.databook.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.filepicker.databook.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
